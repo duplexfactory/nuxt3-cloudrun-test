34f177d40b6545fc848260a4f48ba5acd1669b87
zendagin <zendagin1126@gmail.com>
2022-09-28 18:50:15 +0900

    fs page

---
+++ pages/fs.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  const firestore = await useFetch("/api/firestore");
+  const {collections} = firestore.data.value;
+
+  const firestoreDcs = await useFetch("/api/firestore/docs");
+  const {docs} = firestoreDcs.data.value;
+</script>
+
+<template>
+  <div>
+    <div>
+      cls {{ collections }}
+    </div>
+    <div>
+      docs {{ docs }}
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,10 @@
 <script setup lang="ts">
-  const firestore = await useFetch("/api/firestore");
-  const {collections} = firestore.data.value;
-
-  const firestoreDcs = await useFetch("/api/firestore/docs");
-  const {docs} = firestoreDcs.data.value;
-
   const config = useRuntimeConfig();
   const ts = config.TEST;
 </script>
 
 <template>
   <div>
-    <div>
-      collections {{ collections }}
-    </div>
-    <div>
-      docs {{ docs }}
-    </div>
     <div>
       TEST {{ ts }}
     </div>
