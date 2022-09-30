96db4de7720a0551391a79031166c1b9126b4f67
zendagin <zendagin1126@gmail.com>
2022-09-30 12:15:01 +0900

    pause firebase

--- plugins/fireabase.server.ts
+++ plugins/fireabase.server.ts
@@ -3,12 +3,12 @@ import {defineNuxtPlugin} from "#app";
 
 export default defineNuxtPlugin(({$config}) => {
     if (!getApps().length) {
-        initializeApp({
-            credential: cert({
-                projectId: $config.FIREBASE_PROJECT_ID,
-                clientEmail: $config.FIREBASE_CLIENT_EMAIL,
-                privateKey: $config.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-            })
-        });
+        // initializeApp({
+        //     credential: cert({
+        //         projectId: $config.FIREBASE_PROJECT_ID,
+        //         clientEmail: $config.FIREBASE_CLIENT_EMAIL,
+        //         privateKey: $config.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        //     })
+        // });
     }
 });
